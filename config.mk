d44f3b7afba53dc4cbdef3610ab6b478b52033b6
Kollins <kgl2@cin.ufpe.br>
2022-10-01 19:27:52 -0300

    Makefile update test rules

--- config.mk
+++ config.mk
@@ -9,9 +9,10 @@ INSTALL_PATH := $(DEST)
 CC 			?=
 CFLAGS 		:= -Wall -Werror -pthread
 DBGFLAGS 	:= -g -ggdb
-TESTFLAGS 	:= -fprofile-arcs -ftest-coverage --coverage -lgcov
+TESTFLAGS 	:= -fprofile-arcs -ftest-coverage --coverage
 LINKFLAGS 	:= -shared
 
 COBJFLAGS 	:= $(CFLAGS) -c -fPIC
 test: COBJFLAGS 	+= $(TESTFLAGS)
+test: LINKFLAGS 	+= -fprofile-arcs -lgcov
 debug: COBJFLAGS 	+= $(DBGFLAGS)
